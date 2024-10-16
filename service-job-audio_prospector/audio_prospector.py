f830d02332a8d6249d8a95b421a6bf138aa3197f
routereigrp50 <routeriegrp50@gmail.com>
2024-10-16 12:42:08 +0200

    16.10.2024

---
+++ service-job-audio_prospector/audio_prospector.py
@@ -0,0 +1,39 @@
+from handlers.logging import Logging as h_log
+from handlers.database import Database as h_db
+import threading
+from fastapi import FastAPI, HTTPException, Request
+from fastapi.responses import JSONResponse
+import uvicorn
+
+
+
+class AudioProspector:
+    def __init__(self) -> tuple:
+        self._api = FastAPI(title="Job - Audio Prospector", version="v0.2",openapi_tags=[])
+        self._api_config = None
+        self._api_hit_counter = 1
+        self._api_routing()
+    
+
+    def _api_routing(self) -> None:
+        pass
+
+    def api_set_config(self, configuration: dict) -> None:
+        self._api_config = configuration
+    
+    def api_run(self) -> None:
+        config = uvicorn.Config(self._api, host=self._api_config['ip'], port=self._api_config['port'], log_level="warning")
+        server = uvicorn.Server(config)
+        server.run()
+
+    def _job_raport_status(self, job_status: str, job_config: dict) -> tuple:
+        pass
+
+    def _job_start(self, job_config: dict) -> tuple:
+        pass
+    
+    def _job_stop(self, job_name: str) -> tuple:
+        pass
+    
+    def _job_loop(self) -> None:
+        pass
--- service-job_supervisor/job_supervisor.py
+++ service-job_supervisor/job_supervisor.py
@@ -49,22 +49,38 @@ class JobSupervisor:
             ''' Check if job_name is valid '''
             valid_job_names = ["audioprospector"]
             if job_name not in valid_job_names:
-                h_log.create_log(2, "job_supervisor.JobSupervisor._api_routing", f"ENOK: API Call no. {self._api_hit_counter} - {log_api_endpoint} - Invalid job name")
+                h_log.create_log(3, "job_supervisor.JobSupervisor._api_routing", f"ENOK: API Call no. {self._api_hit_counter} - {log_api_endpoint} - Invalid job name")
                 self._api_hit_counter+=1
                 return JSONResponse(content={"status":"failed","reason":"Invalid job name"}, status_code=400)
             ''' Run job '''
-            job_run_status, job_run_content = self._job_run(job_name, job_config)
-            if not job_run_status:
-                h_log.create_log(2, "job_supervisor.JobSupervisor._api_routing", f"ENOK: API Call no. {self._api_hit_counter} - {log_api_endpoint} - {str(job_run_content)}")
+            job_start_status, job_start_content = self._job_start(job_name, job_config)
+            if not job_start_status:
+                h_log.create_log(2, "job_supervisor.JobSupervisor._api_routing", f"ENOK: API Call no. {self._api_hit_counter} - {log_api_endpoint} - {str(job_start_content)}")
                 self._api_hit_counter+=1
-                return JSONResponse(content={"status":"failed","reason":str(job_run_content)}, status_code=400)
+                return JSONResponse(content={"status":"failed","reason":str(job_start_content)}, status_code=400)
             h_log.create_log(4, "job_supervisor.JobSupervisor._api_routing", f"EOK: API Call no. {self._api_hit_counter} - {log_api_endpoint}")
             self._api_hit_counter+=1
-            return JSONResponse(content={"status":"success","content":job_run_content}, status_code=201)
+            return JSONResponse(content={"status":"success","content":job_start_content}, status_code=201)
         
-        @self._api.post("api/job/stop/<job_name>",tags=["Job operations"])
-        async def stop_job():
-            return {"OK"}
+        @self._api.post("/api/job/stop",tags=["Job operations"])
+        async def stop_job(job_name: str):
+            log_api_endpoint = f"/api/job/start?job_name={job_name}"
+            h_log.create_log(4, "job_supervisor.JobSupervisor._api_routing", f"S: API Call no. {self._api_hit_counter} - {log_api_endpoint}")
+            ''' Check if job_name is valid '''
+            valid_job_names = ["audioprospector"]
+            if job_name not in valid_job_names:
+                h_log.create_log(3, "job_supervisor.JobSupervisor._api_routing", f"ENOK: API Call no. {self._api_hit_counter} - {log_api_endpoint} - Invalid job name")
+                self._api_hit_counter+=1
+                return JSONResponse(content={"status":"failed","reason":"Invalid job name"}, status_code=400)
+            ''' Stop job '''
+            job_stop_status, job_stop_content = self._job_stop(job_name)
+            if not job_stop_status:
+                h_log.create_log(2, "job_supervisor.JobSupervisor._api_routing", f"ENOK: API Call no. {self._api_hit_counter} - {log_api_endpoint} - {str(job_stop_content)}")
+                self._api_hit_counter+=1
+                return JSONResponse(content={"status":"failed","reason":str(job_stop_content)}, status_code=400)
+            h_log.create_log(4, "job_supervisor.JobSupervisor._api_routing", f"EOK: API Call no. {self._api_hit_counter} - {log_api_endpoint}")
+            self._api_hit_counter+=1
+            return JSONResponse(content={"status":"success","content":job_stop_content}, status_code=201)
         
     def api_set_config(self, configuration: dict) -> None:
         self._api_config = configuration
@@ -74,9 +90,17 @@ class JobSupervisor:
         server = uvicorn.Server(config)
         server.run()
     
-    def _job_run(self, job_name: str, job_config: dict) -> tuple:
+    def _job_start(self, job_name: str, job_config: dict) -> tuple:
         match job_name:
             case "audioprospector":
                 return (True, None)
             case _:
                 return (False, "Invalid job name")
+    
+    def _job_stop(self, job_name: str) -> tuple:
+        match job_name:
+            case "audioprospector":
+                return (True, None)
+            case _:
+                return (False, "Invalid job name")
+    
